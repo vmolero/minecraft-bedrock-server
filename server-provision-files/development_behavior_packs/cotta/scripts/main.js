2afef5941f61e95d6ed60ab113769b714b84df46
Victor Molero <victor.molero@gmail.com>
2023-06-14 18:27:14 +0200

    Initial files

---
+++ server-provision-files/development_behavior_packs/cotta/scripts/main.js
@@ -0,0 +1,10 @@
+import { world, system } from "@minecraft/server";
+function mainTick() {
+    if (system.currentTick % 100 === 0) {
+        world.sendMessage("say Hello starter! Tick: " + system.currentTick);
+    }
+    system.run(mainTick);
+}
+system.run(mainTick);
+
+//# sourceMappingURL=../../_cottaDebug/main.js.map
